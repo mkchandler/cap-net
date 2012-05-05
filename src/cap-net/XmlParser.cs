60dd10a7c6eac037084c903ac8edb78b0d81ca45
Matt Chandler <mkchandler@gmail.com>
2012-05-05 17:10:55 -0500

    Added a new parser class.

---
+++ src/cap-net/XmlParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CAP
+{
+    public class XmlParser
+    {
+        public Alert Parse(string xml)
+        {
+            var document = XDocument.Parse(xml);
+            var alert = ParseInternal(document);
+            return alert;
+        }
+
+        private Alert ParseInternal(XDocument document)
+        {
+            var alert = new Alert();
+
+
+
+            return alert;
+        }
+    }
+}
