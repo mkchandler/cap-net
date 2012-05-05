e6fbfe8a80d615570e0ac962d1ca6b658e01f527
Matt Chandler <mkchandler@gmail.com>
2012-05-04 19:46:28 -0500

    Added visual studio solution.

---
+++ cap-net/cap-net/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cap-net")]
+[assembly: AssemblyDescription("A Common Alerting Protocol [de]serialization library for the .NET framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cap-net")]
+[assembly: AssemblyCopyright("Copyright © 2012 Matt Chandler and contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c5cb1c6-0f27-41d7-b3e8-520a75762897")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
