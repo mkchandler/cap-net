f10da6262256c5e2255fe15ebd954d10c45c472f
Matt Chandler <mkchandler@gmail.com>
2012-05-04 21:41:16 -0500

    Added fields to the models based on the spec.

--- src/cap-net/Models/Alert.cs
+++ src/cap-net/Models/Alert.cs
@@ -8,5 +8,35 @@ namespace CAP
 {
     public class Alert
     {
+        public string Identifier { get; set; }
+        public string Sender { get; set; }
+        public DateTime? Sent { get; set; }
+        public string Status { get; set; }
+        public MessageType? MessageType { get; set; }
+        public string Source { get; set; }
+        public Scope? Scope { get; set; }
+        public string Restriction { get; set; }
+        public string Addresses { get; set; }
+        public string Code { get; set; }
+        public string Note { get; set; }
+        public string References { get; set; }
+        public string Incidents { get; set; }
+        public IEnumerable<Info> Infos { get; set; }
+    }
+
+    public enum MessageType
+    {
+        Alert,
+        Update,
+        Cancel,
+        Ack,
+        Error
+    }
+
+    public enum Scope
+    {
+        Public,
+        Restricted,
+        Private
     }
 }
--- src/cap-net/Models/Info.cs
+++ src/cap-net/Models/Info.cs
@@ -8,5 +8,12 @@ namespace CAP
 {
     public class Info
     {
+        private string _language;
+
+        public string Language
+        {
+            get { return String.IsNullOrWhiteSpace(_language) ? "en-US" : _language; }
+            set { _language = value; }
+        }
     }
 }
