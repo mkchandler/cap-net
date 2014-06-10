f1e2dc8d11604a4128817d163a7a19389b24e384
Victor Grigoriu <vgrigoriu@gmail.com>
2014-06-10 10:07:41 +0300

    Add sender

--- src/CAPNet.Tests/XmlCreatorTests.cs
+++ src/CAPNet.Tests/XmlCreatorTests.cs
@@ -7,23 +7,33 @@ namespace CAPNet.Tests
 {
     public class XmlCreatorTests
     {
+        private static readonly Alert Alert = new Alert
+                                                  {
+                                                      Identifier = Guid.NewGuid().ToString(),
+                                                      Sender = "victorg@email.com",
+                                                      Sent = DateTimeOffset.Now,
+                                                      Status = Status.Test,
+                                                      MessageType = MessageType.Alert,
+                                                      Scope = Scope.Private
+                                                  };
+
         [Fact]
         public void XmlNodeReturnedHasCAP12Namespace()
         {
-            var alert = new Alert
-                            {
-                                Identifier = Guid.NewGuid().ToString(),
-                                Sender = "victorg@email.com",
-                                Sent = DateTimeOffset.Now,
-                                Status = Status.Test,
-                                MessageType = MessageType.Alert,
-                                Scope = Scope.Private
-                            };
-
-            XElement xmlElement = XmlCreator.Create(alert);
+            XElement xmlElement = XmlCreator.Create(Alert);
 
             Assert.Equal("urn:oasis:names:tc:emergency:cap:1.2", xmlElement.Name.NamespaceName);
         }
 
+        [Fact]
+        public void XmlNodeReturnedHasSender()
+        {
+            var alertElement = XmlCreator.Create(Alert);
+
+            var senderElement = alertElement.Element(XmlCreator.CAP12Namespace + "sender");
+
+            Assert.NotNull(senderElement);
+            Assert.Equal("victorg@email.com", senderElement.Value);
+        }
     }
 }
--- src/CAPNet/XmlCreator.cs
+++ src/CAPNet/XmlCreator.cs
@@ -1,14 +1,16 @@
 using System.Xml.Linq;
 
-namespace CAPNet.Tests
+namespace CAPNet
 {
     public static class XmlCreator
     {
-        private static XNamespace CAP12Namespace = "urn:oasis:names:tc:emergency:cap:1.2";
+        public static XNamespace CAP12Namespace = "urn:oasis:names:tc:emergency:cap:1.2";
 
         public static XElement Create(Alert alert)
         {
-            return new XElement(CAP12Namespace + "alert");
+            return new XElement(
+                CAP12Namespace + "alert",
+                new XElement(CAP12Namespace + "sender", alert.Sender));
         }
     }
 }
