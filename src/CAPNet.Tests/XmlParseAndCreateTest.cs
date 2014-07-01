7505c3b03fe4b7cce0575e536ec057131c2fc10d
StanculescuDaniel <danstan01@gmail.com>
2014-07-01 16:06:19 +0300

    added cap 1.1 example and new test class

---
+++ src/CAPNet.Tests/XmlParseAndCreateTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace CAPNet.Tests
+{
+    public class XmlParseAndCreateTest
+    {
+        [Fact]
+        public void XmlTest()
+        {
+            
+        }
+    }
+}
