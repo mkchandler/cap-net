59cd824227b508bc7e6957f7bacf5f64d7603b14
StanculescuDaniel <danstan01@gmail.com>
2014-07-02 17:57:03 +0300

    changed Xml Parse and Create tests

--- src/CAPNet.Tests/XmlParseAndCreateTest.cs
+++ src/CAPNet.Tests/XmlParseAndCreateTest.cs
@@ -16,72 +16,120 @@ namespace CAPNet.Tests
         public void OrangeAlertXmlGeneralTest()
         {
             string xmlContent = Xml.OrangeAlertXml;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void MultipleCircleXmlGeneralTest()
         {
             string xmlContent = Xml.MultipleCircleXml;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void MultipleParameterXmlGeneralTest()
         {
             string xmlContent = Xml.MultipleParameterTestXml;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            XElement alertElement = XmlCreator.Create(alert);
-            string alertElementString = alertElement.ToString();
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            //Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void ThunderStorm12AllDatesXmlGeneralTest()
         {
             string xmlContent = Xml.Thunderstorm12AllDatesXml;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void AllElementsFilledXmlGeneralTest()
         {
             string xmlContent = Xml.AllElementsFilledAlert;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void SevereThunderStormCap11GeneralTest()
         {
             string xmlContent = Xml.SevereThundertromCap11;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void HomeLandSecurityAlertCap11GeneralTest()
         {
             string xmlContent = Xml.HomeLandSecurityAlertCap11;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
         [Fact]
         private void EarthquakeCap11GeneralTest()
         {
             string xmlContent = Xml.EarthquakeCap11;
+            XDocument originalDocument = XDocument.Parse(xmlContent);
+            
             Alert alert = XmlParser.Parse(xmlContent).First();
-            string alertElementCreated = XmlCreator.CreateDocumentString(alert);
-            Assert.Equal(alertElementCreated, xmlContent);
+            XElement createdElement = XmlCreator.Create(alert);
+
+            XDocument createdDocument = new XDocument();
+            createdDocument.Add(createdElement);
+            
+            Assert.Equal(createdDocument.ToString(), originalDocument.ToString());
         }
 
     }
