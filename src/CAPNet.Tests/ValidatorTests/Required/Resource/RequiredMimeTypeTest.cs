90fcd1d84da85bbf1160e610e09b890e656b675a
vltadrian <adrian.vultur@gmail.com>
2014-07-03 16:05:13 +0300

    Validator for Resource Required Subelements

---
+++ src/CAPNet.Tests/ValidatorTests/Required/Resource/RequiredMimeTypeTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    public class RequiredMimeTypeTest
+    {
+        [Fact]
+        public void InvalidMimeType()
+        {
+            var alert = ResourceMimeTypeCreator(null);
+
+            var mimeTypeValidator = new MimeTypeRequiredValidator(alert);
+            Assert.False(mimeTypeValidator.IsValid);
+            Assert.Equal(1, mimeTypeValidator.Errors.Count());
+        }
+
+        [Fact]
+        public void ValidMimeType()
+        {
+            var alert = ResourceMimeTypeCreator("MimeTypeResource");
+
+            var mimeTypeValidator = new MimeTypeRequiredValidator(alert);
+            Assert.True(mimeTypeValidator.IsValid);
+            Assert.Equal(0, mimeTypeValidator.Errors.Count());
+        }
+
+        private static Alert ResourceMimeTypeCreator(string mimeType)
+        {
+            var alert = new Alert();
+            var info = InfoCreator.CreateValidInfo();
+            var resource = new Resource();
+            resource.MimeType = mimeType;
+            info.Resources.Add(resource);
+            alert.Info.Add(info);
+            return alert;
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/Resource/MimeTypeRequiredValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MimeTypeRequiredValidator:Validator<Alert>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alert"></param>
+        public MimeTypeRequiredValidator(Alert alert) : base(alert) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get 
+            {
+                var invalidMimeType = from info in Entity.Info
+                                      from resource in info.Resources
+                                      where string.IsNullOrEmpty(resource.MimeType)
+                                      select resource.MimeType;
+                return !invalidMimeType.Any();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                        yield return new MimeTypeRequiredError();
+            }
+        }
+    }
+}
