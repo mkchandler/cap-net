e63b0d7d2ed76c067d93f84ea40bd968afb85076
vltadrian <adrian.vultur@gmail.com>
2014-07-03 17:06:38 +0300

    Type Changes in Resources

---
+++ src/CAPNet.Tests/ValidatorTests/Required/Resource/ResourceDescriptionRequiredValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using CAPNet.Tests;
+using CAPNet.Models;
+
+namespace CAPNet
+{
+    public class ResourceDescriptionRequiredValidatorTests
+    {
+        [Fact]
+        public void ResourceWithEmptyDescriptionIsInvalid()
+        {
+            var resource = new Resource();
+            resource.Description = "";
+
+            var descriptionValidator = new ResourceDescriptionRequiredValidator(resource);
+            Assert.False(descriptionValidator.IsValid);
+            Assert.Equal(1, descriptionValidator.Errors.Count());
+        }
+
+        [Fact]
+        public void ResourceWithDescriptionIsValid()
+        {
+            var resource = new Resource();
+            resource.Description = "description";
+
+            var descriptionValidator = new ResourceDescriptionRequiredValidator(resource);
+            Assert.True(descriptionValidator.IsValid);
+        }
+
+    }
+}
