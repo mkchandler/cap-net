c0bb189c50912f9d90bcb88394d0eadfb5d39482
Victor Grigoriu <vgrigoriu@gmail.com>
2014-07-03 10:14:53 +0300

    Create NamedV alue base class

--- src/CAPNet/Models/EventCode.cs
+++ src/CAPNet/Models/EventCode.cs
@@ -3,43 +3,16 @@
     /// <summary>
     /// 
     /// </summary>
-    public class EventCode
+    public class EventCode : NamedValue
     {
-        private readonly string value;
-
-        private readonly string valueName;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public string Value
-        {
-            get
-            {
-                return this.value;
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public string ValueName
-        {
-            get
-            {
-                return this.valueName;
-            }
-        }
-
         /// <summary>
         /// 
         /// </summary>
         /// <param name="valueName"></param>
         /// <param name="value"></param>
         public EventCode(string valueName, string value)
+            : base(valueName, value)
         {
-            this.valueName = valueName;
-            this.value = value;
         }
     }
 }
