4145f7133832a93a6b225e8bfc3d2304eaa81b0e
vltadrian <adrian.vultur@gmail.com>
2014-07-04 10:37:03 +0300

    Area : Ceiling Existance Validator Added
    Also tests added

---
+++ src/CAPNet.Tests/ValidatorTests/Required/Area/CeilingExistanceValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using CAPNet.Models;
+using System.Linq;
+using Xunit;
+
+namespace CAPNet
+{
+    public class CeilingExistanceValidatorTests
+    {
+        [Fact]
+        public void AreaWithCeilingAndWitAltitudeNullIsInvalid()
+        {
+            var area = new Area();
+            area.Ceiling = 123;
+            area.Altitude = null;
+
+            var ceilingExistanceValidator = new CeilingExistanceValidator(area);
+            Assert.False(ceilingExistanceValidator.IsValid);
+        }
+
+        [Fact]
+        public void AreaWithCeilingAndWitAltitudeIsValid()
+        {
+            var area = new Area();
+            area.Ceiling = 123;
+            area.Altitude = 142;
+
+            var ceilingExistanceValidator = new CeilingExistanceValidator(area);
+            Assert.True(ceilingExistanceValidator.IsValid);
+        }
+    }
+}
---
+++ src/CAPNet/Validator/Alert/Area/CeilingExistanceValidator.cs
@@ -0,0 +1,40 @@
+﻿using CAPNet.Models;
+using System.Collections.Generic;
+
+namespace CAPNet
+{
+    /// <summary>
+    /// ceiling MUST NOT be used except in combination with the altitude element.
+    /// </summary>
+    public class CeilingExistanceValidator : Validator<Area>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="area"></param>
+        public CeilingExistanceValidator(Area area) : base(area) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override IEnumerable<Error> Errors
+        {
+            get
+            {
+                if (!IsValid)
+                    yield return new CeilingExistanceError();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return Entity.Altitude.HasValue && Entity.Ceiling.HasValue;
+            }
+        }
+    }
+}
